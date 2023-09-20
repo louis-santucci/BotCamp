f5b888181b58c27ec0e57764e30fbbdb5dacc634
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-09-20 12:14:00 +0200

    [INIT] first commit

---
+++ src/main/java/com/botcamp/botcamp/BotCampApplication.java
@@ -0,0 +1,13 @@
+package com.botcamp.botcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/botcamp/botcamp/BotCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botcamp.botcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
