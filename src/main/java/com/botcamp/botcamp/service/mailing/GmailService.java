c1d14a7f38bb68c92a297c8b2e120e19a460b0cc
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-10-18 17:26:04 +0200

    [GMAIL SERVICE] Updated Gmail Service interface

--- src/main/java/com/botcamp/botcamp/service/mailing/GmailService.java
+++ src/main/java/com/botcamp/botcamp/service/mailing/GmailService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 public interface GmailService {
-    List<Email> getBCEmails(String beginDate, String endDate) throws IOException;
-    void sendEmail(Email email, String sender, String receiver);
+    List<Email> getEmails(String beginDate, String endDate) throws IOException, InterruptedException;
+
+    List<Email> getEmails(String beginDate, String endDate, String sender, String subject) throws IOException, InterruptedException;
 }
