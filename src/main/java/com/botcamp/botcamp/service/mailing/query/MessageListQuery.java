c621a91318e42f87234cb1c702d764a9b4b20d97
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-10-26 22:51:51 +0200

    [MESSAGE LIST QUERY] Added decorator for getter/setter

--- src/main/java/com/botcamp/botcamp/service/mailing/query/MessageListQuery.java
+++ src/main/java/com/botcamp/botcamp/service/mailing/query/MessageListQuery.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class MessageListQuery implements Query<GmailQueryParameter> {
     private String userEmail;
     private GmailQueryParameter query;
+    private String nextPageToken;
 
     @Override
     public String getUserEmail() {
