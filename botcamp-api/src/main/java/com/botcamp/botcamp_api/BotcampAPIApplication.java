edd0f606e61821f8df567bca4a444f6cae7e497a
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-10-27 22:45:00 +0200

    [BOTCAMP API] Created new module for Botcamp API

---
+++ botcamp-api/src/main/java/com/botcamp/botcamp_api/BotcampAPIApplication.java
@@ -0,0 +1,13 @@
+package com.botcamp.botcamp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotcampAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotcampAPIApplication.class, args);
+    }
+
+}
