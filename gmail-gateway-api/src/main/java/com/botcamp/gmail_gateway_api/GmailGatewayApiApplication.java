96009767770e6a27b9fb473055a6cf98662d89a1
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-10-30 18:57:17 +0100

    [GMAIL GATEWAY API] Renamed class in GmailGatewayApiApplication

--- gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/GmailGatewayApiApplication.java
+++ gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/GmailGatewayApiApplication.java
@@ -1,12 +1,12 @@
 package com.botcamp.gmail_gateway_api;
 
-import com.botcamp.gmail_gateway_api.config.BotCampConfiguration;
+import com.botcamp.gmail_gateway_api.config.GmailGatewayConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(BotCampConfiguration.class)
+@Import(GmailGatewayConfiguration.class)
 public class GmailGatewayApiApplication {
 
     public static void main(String[] args) {
