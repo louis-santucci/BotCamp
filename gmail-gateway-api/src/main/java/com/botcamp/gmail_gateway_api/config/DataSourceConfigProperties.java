c616c593c186390c9f89aa6e1a397d940a6e7a0e
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-10-30 19:17:15 +0100

    [DATA SOURCE CONFIG PROPERTIES] Created DataSourceConfigProperties

---
+++ gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/config/DataSourceConfigProperties.java
@@ -0,0 +1,14 @@
+package com.botcamp.gmail_gateway_api.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "db")
+public class DataSourceConfigProperties {
+    private int port;
+    private String hostname;
+    private String username;
+    private String password;
+    private String dbName;
+}
