52127ab4fddc128751943a0ef4d7d1bb4b040696
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-11-04 16:23:36 +0100

    [GMAIL GATEWAY API] Adding getter for BotcampUser

--- gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/config/BotcampUser.java
+++ gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/config/BotcampUser.java
@@ -1,10 +1,12 @@
 package com.botcamp.gmail_gateway_api.config;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
 
+@Getter
 public class BotcampUser extends User {
 
     private String gmailEmail;
