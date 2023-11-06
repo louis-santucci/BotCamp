1147405fa9b1bea97c1dde6b140c9b930835edaa
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-11-06 17:21:38 +0100

    [BOTCAMP USER CONFIG] Added final to attribute

--- gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/config/BotcampUser.java
+++ gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/config/BotcampUser.java
@@ -9,15 +9,10 @@ import java.util.Collection;
 @Getter
 public class BotcampUser extends User {
 
-    private String gmailEmail;
+    private final String gmailEmail;
 
     public BotcampUser(String username, String password, Collection<? extends GrantedAuthority> authorities, String gmailEmail) {
         super(username, password, authorities);
-        this.gmailEmail = gmailEmail;
-    }
-
-    public BotcampUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities, String gmailEmail) {
-        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
         this.gmailEmail = gmailEmail;
     }
 }
