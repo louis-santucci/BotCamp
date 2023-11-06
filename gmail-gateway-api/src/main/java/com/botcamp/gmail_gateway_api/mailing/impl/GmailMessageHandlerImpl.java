48c4c8cf4541c498d158ee560c668b0f8e6622cd
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-11-06 17:23:14 +0100

    [GMAIL MSG HANDLER] Updated GmailMessageHandlerImpl

--- gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/mailing/impl/GmailMessageHandlerImpl.java
+++ gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/mailing/impl/GmailMessageHandlerImpl.java
@@ -1,9 +1,11 @@
 package com.botcamp.gmail_gateway_api.mailing.impl;
 
 import com.botcamp.gmail_gateway_api.mailing.Email;
+import com.botcamp.gmail_gateway_api.mailing.EmailHandlingException;
 import com.botcamp.gmail_gateway_api.mailing.MessageHandler;
 import com.botcamp.utils.DateUtils;
 import com.google.api.services.gmail.model.Message;
+import com.google.api.services.gmail.model.MessagePart;
 import com.google.api.services.gmail.model.MessagePartHeader;
 import org.springframework.stereotype.Component;
 
@@ -14,34 +16,50 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
+
 @Component
 public class GmailMessageHandlerImpl implements MessageHandler {
     private static final String HEADER_TO = "To";
     private static final String HEADER_SUBJECT = "Subject";
     private static final String HEADER_FROM = "From";
     private static final String HEADER_DATE = "Date";
-    private static final String HEADER_DATETIME_FORMAT = "dd LLL yyyy HH:mm:ss";
 
-    public Email handleMessage(Object msg) {
-        Message message = (Message) msg;
-        byte[] base64Body = message.getPayload().getParts().get(0).getBody().getData().getBytes(StandardCharsets.ISO_8859_1);
-        String body = cleanCrlfEndOfLine(new String(Base64.getUrlDecoder().decode(base64Body)));
-        Map<String, String> headerMap = headerListToMap(message.getPayload().getHeaders());
+    public Email handleMessage(Object msg) throws EmailHandlingException {
+        try {
+            Message message = (Message) msg;
+            Map<String, String> headerMap = headerListToMap(message.getPayload().getHeaders());
+            String receiver = headerMap.get(HEADER_TO);
+            String sender = getEmailFromHeaderValue(headerMap.get(HEADER_FROM));
+            String subject = headerMap.get(HEADER_SUBJECT);
+            String dateTimeStr = DateUtils.cleanDate(headerMap.get(HEADER_DATE));
+            LocalDateTime dateTime = DateUtils.StringToDateTime(dateTimeStr, RFC_1123_DATE_TIME);
+            byte[] base64Body = getMessageBody(message);
+            String base64BodyString = new String(Base64.getUrlDecoder().decode(base64Body));
+            String body = cleanCrlfEndOfLine(base64BodyString);
 
-        String receiver = headerMap.get(HEADER_TO);
-        String sender = getEmailFromHeaderValue(headerMap.get(HEADER_FROM));
-        String subject = headerMap.get(HEADER_SUBJECT);
-        String dateTimeStr = DateUtils.cleanDate(headerMap.get(HEADER_DATE));
-        LocalDateTime dateTime = DateUtils.StringToDateTime(dateTimeStr, HEADER_DATETIME_FORMAT);
+            return Email.builder()
+                    .receiver(receiver)
+                    .subject(subject)
+                    .sender(sender)
+                    .dateTime(dateTime)
+                    .body(body)
+                    .build();
+        } catch (Exception e) {
+            throw new EmailHandlingException(e);
+        }
+    }
 
-        Email email = Email.builder()
-                .receiver(receiver)
-                .subject(subject)
-                .sender(sender)
-                .dateTime(dateTime)
-                .body(body)
-                .build();
-        return email;
+    private static byte[] getMessageBody(Message message) {
+        List<MessagePart> parts = message.getPayload().getParts();
+        byte[] base64Body;
+        if (parts != null) {
+            base64Body = message.getPayload().getParts().get(0).getBody().getData().getBytes(StandardCharsets.ISO_8859_1);
+        } else {
+            base64Body = message.getPayload().getBody().getData().getBytes(StandardCharsets.ISO_8859_1);
+        }
+
+        return base64Body;
     }
 
     private static Map<String, String> headerListToMap(List<MessagePartHeader> list) {
