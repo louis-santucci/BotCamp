bd05448853d6e09cd164f5c614d7b1e8462fe1c4
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-11-07 20:04:38 +0100

    [JWT CONFIG PROPERTIES] Created new POJO for JWT config properties

--- gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/config/properties/JwtConfigProperties.java
+++ gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/config/properties/JwtConfigProperties.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "jwt")
+@ConfigurationProperties(prefix = "security.jwt")
 public class JwtConfigProperties {
     private String secret;
     private Long tokenValidity;
