e488c768a9d7cbf114ccecced4d22c4c0e30bd68
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-11-07 21:39:25 +0100

    [BOTCAMP CONFIG] Adding config classes

---
+++ botcamp-api/src/main/java/com/botcamp/botcamp_api/config/BotcampAPIConfiguration.java
@@ -0,0 +1,11 @@
+package com.botcamp.botcamp_api.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableConfigurationProperties
+public class BotcampAPIConfiguration {
+}
