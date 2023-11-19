631c2eee2a51c5e99bf0b7d261c25483045ac6c6
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-11-19 14:59:50 +0100

    [EMAIL EXCEPTION] Created Email Parsing Exception + Email Sending Exception

---
+++ botcamp-common/src/main/java/com/botcamp/common/exception/EmailParsingException.java
@@ -0,0 +1,7 @@
+package com.botcamp.common.exception;
+
+public class EmailParsingException extends Exception {
+    public EmailParsingException(String message) {
+        super(message);
+    }
+}
