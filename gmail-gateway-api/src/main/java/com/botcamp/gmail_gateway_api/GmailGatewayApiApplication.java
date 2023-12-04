c3e741ce3c92fe32d2a80f62e74d59c94ecd8b3b
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-04 14:47:02 +0100

    [GMAIL GATEWAY API] Updated main class for gmail gateway api

--- gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/GmailGatewayApiApplication.java
+++ gmail-gateway-api/src/main/java/com/botcamp/gmail_gateway_api/GmailGatewayApiApplication.java
@@ -1,16 +1,24 @@
 package com.botcamp.gmail_gateway_api;
 
+import com.botcamp.common.utils.ApplicationUtils;
 import com.botcamp.gmail_gateway_api.config.GmailGatewayConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
+
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @Import(GmailGatewayConfiguration.class)
+@Slf4j
 public class GmailGatewayApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GmailGatewayApiApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication application = new SpringApplication(GmailGatewayApiApplication.class);
+        ConfigurableApplicationContext context = application.run(args);
+        ApplicationUtils.printAccessUrl(context, log);
     }
 
 }
