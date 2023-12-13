48177638d1813232e404d1378481e89e3e9190fe
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-13 21:13:19 +0100

    [GMAIL GATEWAY TEST] Adding MockBeans in TestConfiguration

--- gmail-gateway-api/src/test/java/com/botcamp/gmail_gateway_api/configuration/TestConfiguration.java
+++ gmail-gateway-api/src/test/java/com/botcamp/gmail_gateway_api/configuration/TestConfiguration.java
@@ -1,14 +1,33 @@
 package com.botcamp.gmail_gateway_api.configuration;
 
+import com.botcamp.gmail_gateway_api.controller.MailController;
+import com.botcamp.gmail_gateway_api.credentials.GmailCredentialProvider;
+import com.botcamp.gmail_gateway_api.service.GmailCredentialsService;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.services.gmail.Gmail;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @org.springframework.boot.test.context.TestConfiguration
 @EnableScheduling
 public class TestConfiguration {
-    @Bean
-    Gmail gmailTest() {
-        return null;
-    }
+
+    @MockBean
+    private Gmail gmail;
+
+    @MockBean
+    private MailController mailController;
+
+    @MockBean
+    private GoogleClientSecrets googleClientSecrets;
+
+    @MockBean
+    private GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow;
+
+    @MockBean
+    private GmailCredentialsService gmailCredentialsService;
+
+    @MockBean
+    private GmailCredentialProvider gmailCredentialProvider;
 }
