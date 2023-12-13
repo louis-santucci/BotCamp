b115a9101b97072eff2fcc50d200a478716b9c2a
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-13 20:29:19 +0100

    [GMAIL GATEWAY JWT UTILS] Updated JWTUtils and JwtUserDetailsService

--- botcamp-common/src/main/java/com/botcamp/common/service/JwtUserDetailsService.java
+++ botcamp-common/src/main/java/com/botcamp/common/service/JwtUserDetailsService.java
@@ -1,10 +1,10 @@
 package com.botcamp.common.service;
 
 import com.botcamp.common.entity.AEntity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface JwtUserDetailsService<ENTITY extends AEntity> extends UserDetailsService {
     ENTITY save(Object user);
 
+    String getUsernameFromToken(String token);
 }
