529075beb5e2f5e063b3d71db3052b8e55c6f95f
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-19 23:28:29 +0100

    [JWT TOKEN] Adding JwtToken POJO

---
+++ botcamp-common/src/main/java/com/botcamp/common/jwt/JwtToken.java
@@ -0,0 +1,20 @@
+package com.botcamp.common.jwt;
+
+import com.botcamp.common.response.JwtResponse;
+import com.botcamp.common.utils.DateUtils;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class JwtToken {
+    private String token;
+    private LocalDateTime expiresAt;
+
+    public JwtToken(JwtResponse jwtResponse) {
+        this.token = jwtResponse.getJwtToken();
+        this.expiresAt = DateUtils.stringToDateTime(jwtResponse.getExpiresAt(), DateUtils.formatter);
+    }
+}
