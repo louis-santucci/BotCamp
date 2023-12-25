a63f5607b253ff1daef762c6ea492214fc377a6a
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-26 00:45:28 +0100

    [BTC API] Adding new endpoints to Botcamp API

--- botcamp-common/src/main/java/com/botcamp/common/endpoints/BotcampApiEndpoint.java
+++ botcamp-common/src/main/java/com/botcamp/common/endpoints/BotcampApiEndpoint.java
@@ -8,10 +8,12 @@ public class BotcampApiEndpoint {
     // Controller names
     public static final String JWT_AUTH_CONTROLLER = "JWT Auth Controller";
     public static final String REPORTING_CONTROLLER = "Reporting Controller";
+    public static final String EXECUTION_CONTROLLER = "Execution Controller";
 
     // Controllers paths
     public static final String API_AUTH = "/api/auth";
     public static final String API_REPORTING = "/api/reporting";
+    public static final String API_EXECUTION = "/api/execution";
 
     // JWT Auth Controller
     public static final String AUTH = "/authenticate";
@@ -19,4 +21,7 @@ public class BotcampApiEndpoint {
     // Reporting Controller
     public static final String GENERATE = "/generate";
     public static final String SCHEDULE = "/schedule";
+
+    // Execution Controller
+    public static final String EXECUTION_GET = "/get";
 }
