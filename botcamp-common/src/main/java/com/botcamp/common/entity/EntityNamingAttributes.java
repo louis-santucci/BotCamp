9de952f7bb7e98a14571286eaae75aea50713bb8
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-26 00:52:38 +0100

    [ENTITY NAMING ATTRIBUTES] Added EntityNamingAttributes

---
+++ botcamp-common/src/main/java/com/botcamp/common/entity/EntityNamingAttributes.java
@@ -0,0 +1,37 @@
+package com.botcamp.common.entity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EntityNamingAttributes {
+
+    // Gateway User
+    public static final String GATEWAY_USER = "gateway_user";
+    public static final String GATEWAY_USER_USERNAME = "username";
+    public static final String GATEWAY_USER_PASSWORD = "password";
+    public static final String GATEWAY_USER_AUTHORIZATIONS = "authorizations";
+    public static final String GATEWAY_USER_GMAIL_EMAIL = "gmail_email";
+
+    // Botcamp User
+    public static final String BOTCAMP_USER = "botcamp_user";
+    public static final String BOTCAMP_USER_USERNAME = "username";
+    public static final String BOTCAMP_USER_PASSWORD = "password";
+    public static final String BOTCAMP_USER_AUTHORIZATIONS = "authorizations";
+
+    // Execution
+    public static final String EXECUTION = "execution";
+    public static final String EXECUTION_TYPE = "execution_type";
+    public static final String EXECUTION_STATUS = "status";
+    public static final String EXECUTION_QUERY_PARAMETER = "query_parameter";
+
+    // Job
+    public static final String JOB = "job";
+    public static final String NAME = "name";
+    public static final String ENABLED = "enabled";
+    public static final String CRON = "cron";
+    public static final String SUBJECT = "cron";
+    public static final String JOB_ID = "job_id";
+    public static final String RECIPIENT_LIST = "recipient_list";
+    public static final String RECIPIENT = "recipient";
+}
