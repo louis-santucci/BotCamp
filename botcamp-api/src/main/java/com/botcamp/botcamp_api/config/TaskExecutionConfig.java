050f1204deb7d78a7890774f86b98af45a230c81
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-29 17:14:32 +0100

    [BOTCMAP TASK EXEC CONFIG] Updated new TaskExecutionConfig for Shedlock

--- botcamp-api/src/main/java/com/botcamp/botcamp_api/config/TaskExecutionConfig.java
+++ botcamp-api/src/main/java/com/botcamp/botcamp_api/config/TaskExecutionConfig.java
@@ -2,17 +2,24 @@ package com.botcamp.botcamp_api.config;
 
 import com.botcamp.botcamp_api.config.properties.TaskExecutorConfigProperties;
 import com.botcamp.botcamp_api.config.properties.TaskSchedulerConfigProperties;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Import({
         TaskExecutorConfigProperties.class,
         TaskSchedulerConfigProperties.class
 })
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 public class TaskExecutionConfig {
 
     private static final String TASK_SCHEDULER = "task-scheduler";
@@ -36,5 +43,14 @@ public class TaskExecutionConfig {
         executor.setQueueCapacity(executor.getQueueCapacity());
         executor.setThreadNamePrefix(TASK_EXECUTOR);
         return executor;
+    }
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(JdbcTemplateLockProvider.Configuration.builder()
+                .withJdbcTemplate(new JdbcTemplate(dataSource))
+                .usingDbTime()
+                .withDbUpperCase(true)
+                .build());
     }
 }
