d9f0f511dd8bb815b4a7e014cf057cd123148161
Louis SANTUCCI <louissantucci1998@gmail.com>
2023-12-29 17:12:01 +0100

    [TASK EXECUTOR] Added new TaskExecutor + impl

--- botcamp-api/src/main/java/com/botcamp/botcamp_api/service/TaskExecutor.java
+++ botcamp-api/src/main/java/com/botcamp/botcamp_api/service/TaskExecutor.java
@@ -3,10 +3,11 @@ package com.botcamp.botcamp_api.service;
 import com.botcamp.botcamp_api.execution.Execution;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public interface TaskExecutor {
 
-    void poll();
+    void poll() throws ExecutionException, InterruptedException;
 
-    void executeTasks(List<Execution> executionList);
+    void executeTasks(List<Execution> executionList) throws InterruptedException, ExecutionException;
 }
--- botcamp-api/src/main/java/com/botcamp/botcamp_api/service/impl/TaskExecutorImpl.java
+++ botcamp-api/src/main/java/com/botcamp/botcamp_api/service/impl/TaskExecutorImpl.java
@@ -1,19 +1,29 @@
 package com.botcamp.botcamp_api.service.impl;
 
+import com.botcamp.botcamp_api.config.properties.ExportProperties;
 import com.botcamp.botcamp_api.execution.Execution;
-import com.botcamp.botcamp_api.execution.callable.LogCallable;
+import com.botcamp.botcamp_api.execution.callable.GmailEmailHandlerCallable;
+import com.botcamp.botcamp_api.execution.callable.exporter.CallableExporter;
+import com.botcamp.botcamp_api.execution.callable.exporter.EmailExporter;
+import com.botcamp.botcamp_api.execution.callable.exporter.ExportType;
+import com.botcamp.botcamp_api.execution.callable.exporter.FileExporter;
+import com.botcamp.botcamp_api.service.EmailSender;
+import com.botcamp.botcamp_api.service.GmailGatewayClient;
 import com.botcamp.botcamp_api.service.TaskExecutionService;
 import com.botcamp.botcamp_api.service.TaskExecutor;
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Future;
 
 import static com.botcamp.botcamp_api.execution.ExecutionStatus.*;
 import static com.botcamp.common.request.SortingOrderParameter.ASCENDING;
@@ -24,16 +34,39 @@ public class TaskExecutorImpl implements TaskExecutor {
 
     private final TaskExecutionService taskExecutionService;
     private final ThreadPoolTaskExecutor executor;
+    private final List<CallableExporter> exporters;
+    private final GmailGatewayClient client;
+    private final EmailSender emailSender;
 
     public TaskExecutorImpl(TaskExecutionService executionService,
-                            @Qualifier("botcampExecutor") ThreadPoolTaskExecutor executor) {
+                            @Qualifier("botcampExecutor") ThreadPoolTaskExecutor executor,
+                            ExportProperties exportProperties,
+                            GmailGatewayClient client,
+                            EmailSender emailSender) throws IOException {
         this.taskExecutionService = executionService;
         this.executor = executor;
+        this.exporters = initExporters(exportProperties);
+        this.client = client;
+        this.emailSender = emailSender;
+    }
+
+    List<CallableExporter> initExporters(ExportProperties exportProperties) throws IOException {
+        List<CallableExporter> exporterList = new ArrayList<>();
+        for (ExportType type : exportProperties.getTypes()) {
+            if (type == ExportType.FILE_EXPORT) {
+                exporterList.add(new FileExporter(exportProperties, taskExecutionService));
+            } else if (type == ExportType.EMAIL_EXPORT) {
+                exporterList.add(new EmailExporter(exportProperties, emailSender));
+            }
+        }
+
+        return exporterList;
     }
 
     @Override
     @Scheduled(cron = "${task-executor.polling-cron}")
-    public void poll() {
+    @SchedulerLock(name = "taskExecutionPolling")
+    public void poll() throws ExecutionException, InterruptedException {
         List<Execution> pendingExecutions = taskExecutionService.getExecutions(
                 null,
                 null,
@@ -49,17 +82,20 @@ public class TaskExecutorImpl implements TaskExecutor {
     }
 
     @Override
-    public void executeTasks(List<Execution> pendingExecutions) {
+    public void executeTasks(List<Execution> pendingExecutions) throws InterruptedException, ExecutionException {
         int successfulExecutions = 0;
         for (Execution exec : pendingExecutions) {
             try {
-                executor.submit(new LogCallable(exec)).get();
-                this.taskExecutionService.updateExecution(exec.getId(), SUCCESS);
-                successfulExecutions++;
+                taskExecutionService.updateExecutionStatus(exec.getId(), RUNNING);
+                Future<Void> future = executor.submit(new GmailEmailHandlerCallable(exec, client, exporters));
+                future.get();
             } catch (InterruptedException | ExecutionException e) {
                 log.error("Error while executing callable: {}", e.getMessage());
-                this.taskExecutionService.updateExecution(exec.getId(), FAILURE);
+                this.taskExecutionService.updateExecutionStatus(exec.getId(), FAILURE);
+                throw e;
             }
+            this.taskExecutionService.updateExecutionStatus(exec.getId(), SUCCESS);
+            successfulExecutions++;
         }
         log.info("Successfully executed {} executions", successfulExecutions);
     }
